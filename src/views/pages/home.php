d11a86cd22247c0939ae3235a7d7a97259181cbb
Giovane Richard <giovanerichard@gmail.com>
2022-10-11 11:14:39 -0300

    Implements create

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -2,4 +2,28 @@
 
 <a href="<?=$base;?>/novo">Novo Usuário</a>
 
+<table border="1" width="100%">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>NOME</th>
+            <th>E-MAIL</th>
+            <th>AÇÕES</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($usuarios as $usuario): ?>
+            <tr>
+                <td><?=$usuario['id']; ?></td>
+                <td><?=$usuario['name']; ?></td>
+                <td><?=$usuario['email']; ?></td>
+                <td>
+                    <a href=<?= ?>">[ Editar ]</a>
+                    <a href="">[ Excluir ]</a>
+                </td>
+            </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
+
 <?php $render('footer'); ?>
